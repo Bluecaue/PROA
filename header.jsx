c0e62fa50f7c99bae4fd031e19dfe524ac9c6cd3
Caue Costa <86437122+Bluecaue@users.noreply.github.com>
2024-05-09 13:04:34 -0300

    Add files via upload

---
+++ header.jsx
@@ -0,0 +1,71 @@
+import { Link } from 'react-router-dom';
+import {useState} from "react";
+
+import Logo from '../imagens/logo_sul.png'
+import './HeaderModule.css'
+import Bandeira from '../imagens/header/Brazil.png';
+import LogoHeader from '../imagens/header/Logo_Header.png';
+
+function Header() {
+  const [active, setMode] = useState(false);
+  const ToggleMode = () => {
+    setMode(!active)
+  }
+
+  return (
+    <header>
+      <Link to="/">
+        <div className="esquerda">
+          <img className="logo" src={LogoHeader} alt="Logo Sul Solidário" />
+        </div>
+      </Link>
+      {/* Menu de navegação */}
+      <nav className='nav-list'>
+        <ul className="nav-itens">
+          <li>
+            <Link to='/'>Inicio</Link>
+          </li>
+          <li>
+            <Link to='/Categoria/animais'>Onde doar</Link>
+          </li>
+
+          <li>
+            <Link to='/Sobre'>Sobre a Causa</Link>
+          </li>
+
+          <li>
+            <Link to='/Noticias'>Noticias</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="direita">
+        <img className="bandeira" src={Bandeira} alt="Bandeira do Brasil" />
+      </div>
+
+      {/* Menu de navegação responsivo */}
+      <div className='Modal'>
+        <div className={active ? "icon iconActive" : "icon"} onClick={ToggleMode}>
+          <div className='hamburguer hamburguerIcon'></div>
+        </div>
+
+        <div className={active ?  "menu menuOpen" : "menu , menuClose"}>
+          <div className='list'>
+        <ul className="listItems">
+          <li><Link to='/'>Inicio</Link></li>
+          <li> <Link to='/Categoria/animais'>Onde doar</Link></li>
+          <li><Link to='/Sobre'>Sobre a Causa</Link></li>
+          <li><Link to='/Noticias'>Noticias</Link></li>
+          <div>
+          <img src={Logo} alt="Logo da sul solidário" className='logo' />
+        </div>
+        </ul>
+        
+      </div>
+      </div>
+      </div>
+    </header>
+
+
+  )
+}
+export default Header
